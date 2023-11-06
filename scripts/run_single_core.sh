5558b13e0b8d9951ab74e713e07edcdd527dcbc5
Alexandre V. Jamet <alexandre.jamet@bsc.es>
2023-11-06 14:29:10 +0100

    Updates to the scripts.

--- scripts/run_single_core.sh
+++ scripts/run_single_core.sh
@@ -40,55 +40,54 @@ idx=0
 echo $TRACE_DIR
 
 for trace in ${TRACE_DIR}/*.*.sdc-*.xz; do
-    filename=$(basename -- $trace)
+  filename=$(basename -- $trace)
 
-
-	TRACES_ARRAY[$idx]=$filename
-	idx=$((idx + 1))
+  TRACES_ARRAY[$idx]=$filename
+  idx=$((idx + 1))
 done
 unset idx
 
 echo ${TRACES_ARRAY[@]}
 
 # ChampSim configurations.
-CONFIGS=(	
-	"baseline_cascade_lake_no_prefetchers"
+CONFIGS=(
+  "baseline_cascade_lake_no_prefetchers"
 
-	"baseline_cascade_lake_ipcp"
-	"baseline_cascade_lake_ipcp_spp_ppf"
-	"baseline_cascade_lake_ipcp_hermes_o" 
-	"baseline_cascade_lake_ipcp_spp_ppf_hermes_o"
-	"baseline_cascade_lake_ipcp_hermes_o_double"   
-	"baseline_cascade_lake_ipcp_spp_ppf_tlp_layered_core_l1d_f20_-25"
-	"baseline_cascade_lake_ipcp_iso_prefetcher"
-	
-	"baseline_cascade_lake_berti"
-	"baseline_cascade_lake_berti_spp_ppf"
-	"baseline_cascade_lake_berti_hermes_o"
-	"baseline_cascade_lake_berti_spp_ppf_hermes_o"
-	"baseline_cascade_lake_berti_hermes_o_double" 
-	"baseline_cascade_lake_berti_tlp_layered_core_l1d_f20_-25"
-	"baseline_cascade_lake_berti_iso_prefetcher"
+  "baseline_cascade_lake_ipcp"
+  "baseline_cascade_lake_ipcp_spp_ppf"
+  "baseline_cascade_lake_ipcp_hermes_o"
+  "baseline_cascade_lake_ipcp_spp_ppf_hermes_o"
+  "baseline_cascade_lake_ipcp_hermes_o_double"
+  "baseline_cascade_lake_ipcp_spp_ppf_tlp_layered_core_l1d_f20_-25"
+  "baseline_cascade_lake_ipcp_iso_prefetcher"
+
+  "baseline_cascade_lake_berti"
+  "baseline_cascade_lake_berti_spp_ppf"
+  "baseline_cascade_lake_berti_hermes_o"
+  "baseline_cascade_lake_berti_spp_ppf_hermes_o"
+  "baseline_cascade_lake_berti_hermes_o_double"
+  "baseline_cascade_lake_berti_tlp_layered_core_l1d_f20_-25"
+  "baseline_cascade_lake_berti_iso_prefetcher"
 )
 
 BINARIES=(
-    "1_cores_cascade_lake_800mtps"
+  "1_cores_cascade_lake_800mtps"
 
-    "1_cores_cascade_lake_800mtps"
-    "1_cores_cascade_lake_800mtps"
-    "1_cores_cascade_lake_hermes_o_800mtps"
-    "1_cores_cascade_lake_hermes_o_800mtps"
-    "1_cores_cascade_lake_hermes_o_800mtps"
-    "1_cores_cascade_lake_tlp_800mtps"
-    "1_cores_cascade_lake_800mtps"
+  "1_cores_cascade_lake_800mtps"
+  "1_cores_cascade_lake_800mtps"
+  "1_cores_cascade_lake_hermes_o_800mtps"
+  "1_cores_cascade_lake_hermes_o_800mtps"
+  "1_cores_cascade_lake_hermes_o_800mtps"
+  "1_cores_cascade_lake_tlp_800mtps"
+  "1_cores_cascade_lake_800mtps"
 
-    "1_cores_cascade_lake_800mtps"
-    "1_cores_cascade_lake_800mtps"
-    "1_cores_cascade_lake_hermes_o_800mtps"
-    "1_cores_cascade_lake_hermes_o_800mtps"
-    "1_cores_cascade_lake_hermes_o_800mtps"
-    "1_cores_cascade_lake_tlp_800mtps"
-    "1_cores_cascade_lake_800mtps"
+  "1_cores_cascade_lake_800mtps"
+  "1_cores_cascade_lake_800mtps"
+  "1_cores_cascade_lake_hermes_o_800mtps"
+  "1_cores_cascade_lake_hermes_o_800mtps"
+  "1_cores_cascade_lake_hermes_o_800mtps"
+  "1_cores_cascade_lake_tlp_800mtps"
+  "1_cores_cascade_lake_800mtps"
 )
 
 echo ${CONFIGS[@]}
@@ -96,31 +95,31 @@ echo ${CONFIGS[@]}
 idx=0
 
 for config in ${CONFIGS[@]}; do
-    for trace in ${TRACES_ARRAY[@]}; do
-        JOB_NAME="${config}-${trace}"
-        OUTPUT_FILE="${OUTPUT_DIR}/${config}/${trace}.txt"
-        ERROR_FILE="${OUTPUT_DIR}/${config}/${trace}.err"
+  for trace in ${TRACES_ARRAY[@]}; do
+    JOB_NAME="${config}-${trace}"
+    OUTPUT_FILE="${OUTPUT_DIR}/${config}/${trace}.txt"
+    ERROR_FILE="${OUTPUT_DIR}/${config}/${trace}.err"
 
-	should_wait_ret_code="1"
-        sbatch_ret_code="1"
+    should_wait_ret_code="1"
+    sbatch_ret_code="1"
 
-        # Waiting for the queue to empty a bit.
-        while (($should_wait_ret_code != 0)); do
-            count=$(jobs_in_queue)
-            should_wait_ret_code=$(should_wait $count 10000)
-        done
-
-    	# Creating the output directory if doesn't exist yet.
-    	mkdir -p ${OUTPUT_DIR}/${config}
-
-        # Launching the job ensuring that it has been taken into account.
-        while (($sbatch_ret_code != 0)); do
-            sbatch --job-name=${JOB_NAME} --output=${OUTPUT_FILE} --error=${ERROR_FILE} --chdir=${WORKING_DIR} --requeue --mail-type=FAIL,TIME_LIMIT --mail-user=alexandre.jamet@bsc.es ${WORKING_DIR}/scripts/run_single_core.job config/${config}.json ${BINARIES[$idx]} ${trace}
-            sbatch_ret_code=$?
-        done
+    # Waiting for the queue to empty a bit.
+    while (($should_wait_ret_code != 0)); do
+      count=$(jobs_in_queue)
+      should_wait_ret_code=$(should_wait $count 10000)
     done
 
-    # Incrementing the index counter.
-    idx=$((idx + 1))
+    # Creating the output directory if doesn't exist yet.
+    mkdir -p ${OUTPUT_DIR}/${config}
+
+    # Launching the job ensuring that it has been taken into account.
+    while (($sbatch_ret_code != 0)); do
+      sbatch --job-name=${JOB_NAME} --output=${OUTPUT_FILE} --error=${ERROR_FILE} --chdir=${WORKING_DIR} --requeue --mail-type=FAIL,TIME_LIMIT --mail-user=alexandre.jamet@bsc.es ${WORKING_DIR}/scripts/run_single_core.job config/${config}.json ${BINARIES[$idx]} ${trace}
+      sbatch_ret_code=$?
+    done
+  done
+
+  # Incrementing the index counter.
+  idx=$((idx + 1))
 done
 unset idx
